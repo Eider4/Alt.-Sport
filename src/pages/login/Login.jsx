6a714799a5e10d929cdfe01f74b728f4eb6c8e78
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-04 12:00:12 -0500

    subir el proyecto a vercel

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import useStoreUsuario from "../../store/manageUser";
 function Login() {
   const [nombre, setNombre] = useState("");
   const createUser = useStoreUsuario((state) => state.createUser);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     createUser(nombre);
@@ -11,16 +12,25 @@ function Login() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e) => setNombre(e.target.value)}
-          value={nombre}
-          type="text"
-          placeholder="Nombre"
-        />
-        <input type="submit" placeholder="Guardar" />
-      </form>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+            type="text"
+            placeholder="Nombre"
+            className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-green-300"
+          />
+          <button
+            type="submit"
+            className="w-full p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-200"
+          >
+            Guardar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
